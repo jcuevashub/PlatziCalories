f1eea8b1902e6cb7807bbb39a90895fffec3661c
Jackson Cuevas <jackson.cuevas@hotmail.com>
2025-03-10 08:40:03 -0400

    Calorie Tracker: selector de días y comidas

--- app/src/main/java/com/example/platzicalories/app/presentation/tracker_overview/TrackerOverviewScreen.kt
+++ app/src/main/java/com/example/platzicalories/app/presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -3,27 +3,83 @@ package com.example.platzicalories.app.presentation.tracker_overview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.platzicalories.app.presentation.tracker_overview.components.DaySelector
+import com.example.platzicalories.app.presentation.tracker_overview.components.ExpandableMeal
+import com.example.platzicalories.app.presentation.tracker_overview.components.NutrientsHeader
+import com.example.platzicalories.app.presentation.tracker_overview.model.defaultMeals
+import com.example.platzicalories.app.ui.theme.LocalSpacing
 import com.example.platzicalories.app.ui.theme.PlatziCaloriesTheme
+import java.time.LocalDate
 
 @Composable
 fun TrackerOverviewScreen(
     trackerOverviewViewModel: TrackerOverviewViewModel = hiltViewModel()
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Tracker Overview Screen"
+    val spacing = LocalSpacing.current
+    val context = LocalContext.current
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeader()
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(defaultMeals){ meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun TrackerOverviewScreenText() {
+    val spacing = LocalSpacing.current
+    val context = LocalContext.current
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeader()
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = LocalDate.now(),
+                onPreviousDayClick = {},
+                onNextDayClick = {},
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
             )
         }
     }
@@ -33,6 +89,6 @@ fun TrackerOverviewScreen(
 @Composable
 private fun TrackerOverviewScreenPreview() {
     PlatziCaloriesTheme {
-        TrackerOverviewScreen()
+        TrackerOverviewScreenText()
     }
 }
---
+++ app/src/main/java/com/example/platzicalories/app/presentation/tracker_overview/components/ParseDateText.kt
@@ -0,0 +1,17 @@
+package com.example.platzicalories.app.presentation.tracker_overview.components
+
+import androidx.compose.runtime.Composable
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
+
+@Composable
+fun ParseDateText(date: LocalDate): String {
+    val today = LocalDate.now()
+    return when(date) {
+        today -> "Today"
+        today.minusDays(1) -> "Yesterday"
+        today.plusDays(1) -> "Tomorrow"
+        else -> DateTimeFormatter.ofPattern("dd LLLL").format(date)
+    }
+}
